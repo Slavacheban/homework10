46bc55dd7f3a87ba18d55f98f1690d93c6316781
Slava <slavacheban22@gmail.com>
2019-03-27 17:39:31 +0200

    fix tests

--- src/test/java/homework10/UtilTest.java
+++ src/test/java/homework10/UtilTest.java
@@ -8,18 +8,38 @@ public class UtilTest {
 
     @Test
     public void checkPhoneNumber() {
-
+        boolean expected = true;
+        boolean actualF = Util.checkPhoneNumber("+38(063)-579-42-77");
+        boolean actualS = Util.checkPhoneNumber("063-579-42-77");
+        assertEquals(expected, actualF);
+        assertEquals(expected, actualS);
     }
 
     @Test
     public void checkEmail() {
+        boolean expected = true;
+        boolean actualF = Util.checkEmail("cheban.sl@mail.ru");
+        boolean actualS = Util.checkEmail("slavacheban22@gmail.com");
+        assertEquals(expected, actualF);
+        assertEquals(expected, actualS);
     }
 
     @Test
     public void checkDate() {
+        boolean expected = true;
+        boolean actualF = Util.checkDate("1.12.89");
+        boolean actualS = Util.checkDate("04.04.1993");
+        assertEquals(expected, actualF);
+        assertEquals(expected, actualS);
     }
 
     @Test
     public void checkIP() {
+        boolean expected = true;
+        boolean actualF = Util.checkIP("225.225.89.224");
+        boolean actualS = Util.checkIP("0.0.199.235");
+        assertEquals(expected, actualF);
+        assertEquals(expected, actualS);
     }
+
 }
