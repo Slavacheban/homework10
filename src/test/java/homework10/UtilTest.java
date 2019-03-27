48e3fd9e04fe9f5b0ddee8b2e648b7f0c077439b
Slava <slavacheban22@gmail.com>
2019-03-27 17:26:15 +0200

    added HW10

---
+++ src/test/java/homework10/UtilTest.java
@@ -0,0 +1,25 @@
+package homework10;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UtilTest {
+
+    @Test
+    public void checkPhoneNumber() {
+
+    }
+
+    @Test
+    public void checkEmail() {
+    }
+
+    @Test
+    public void checkDate() {
+    }
+
+    @Test
+    public void checkIP() {
+    }
+}
